50aa213175e7e4cba685de1622339a39f633aa83
SamAng219255 <samang219255@gmail.com>
2017-11-13 21:35:44 +0000

    updated

--- shapes.py
+++ shapes.py
@@ -12,10 +12,20 @@ def order(inx,iny,inz,axis):
  facings=[(inx,iny,inz),(inx,inz,iny),(iny,inz,inx)]
  return facings[axis]
 
-def cylinder(X,Y,Z,axis,block):
- x,y,z=order(X+1,Y+1,Z+1,axis)
+def cylinder(x,y,z,axis,block):
+ x,y,z=x+1,y+1,z+1
  mc=Minecraft.create("127.0.0.1",4711)
  px,py,pz=mc.player.getPos()
- for i in range(-x+1,x):
-  mc.setBlocks(px+i,py-cwm(i,x,y),pz-z+1,px+i,py+cwm(i,x,y),pz+z-1,block)
+ if axis==0:
+  for i in range(-x+1,x):
+   mc.setBlocks(px+i,py-cwm(i,x,y),pz-z+1,px+i,py+cwm(i,x,y),pz+z-1,block)
+ elif axis==1:
+  for i in range(-x+1,x):
+   mc.setBlocks(px+i,py-y+1,pz-cwm(i,x,y),px+i,py+y-1,pz+cwm(i,x,y),block)
+ elif axis==2:
+  for i in range(-y+1,y):
+   mc.setBlocks(px-x+1,py+i,pz-cwm(i,x,y),px+x-1,py+i,pz+cwm(i,x,y),block)
  mc.player.setPos(px,py+y,pz)
+
+def sphere(X,Y,Z,block):
+ print("Not yet implemented.")
